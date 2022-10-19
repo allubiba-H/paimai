94350cf8107492034797617851bef1b783bed2f8
allubiba-H <spider_h@qq.com>
2022-10-19 09:18:08 +0800

    websocket

--- paipai/src/main/java/com/paipai/config/MyConfig.java
+++ paipai/src/main/java/com/paipai/config/MyConfig.java
@@ -2,6 +2,8 @@ package com.paipai.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -24,7 +26,6 @@ public class MyConfig implements WebMvcConfigurer {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new LoginInterception())
@@ -34,5 +35,12 @@ public class MyConfig implements WebMvcConfigurer {
     @Bean
     public ServerEndpointExporter serverEndpointExporter () {
        return new ServerEndpointExporter();
+    }
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(20);
+        taskScheduler.initialize();
+        return taskScheduler;
     }
 }
--- paipai/src/main/java/com/paipai/paipai/util/WebSocketServer.java
+++ paipai/src/main/java/com/paipai/paipai/util/WebSocketServer.java
@@ -13,13 +13,14 @@ import javax.websocket.server.ServerEndpoint;
 public class WebSocketServer {
 
     @OnOpen
-    public void openSocket(@PathVariable("id")Integer id, Session session) {
+    public void openSocket(@PathVariable("id") Integer id, Session session) {
         session.getAsyncRemote().sendText("服务器收到客户端请求");
     }
     @OnMessage
     public void receiveMessage(Session session,String message) {
         System.out.println("session" + session );
     }
+
 
 
 }
